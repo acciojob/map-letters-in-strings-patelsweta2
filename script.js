6e16d500c48cd39b1144d1918116b66f56b7ec1f
patelsweta2 <80040744+patelsweta2@users.noreply.github.com>
2024-04-19 15:19:31 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,19 @@
 //your JS code here. If required.
+function mapLetters(word) {
+	const letterMap = {};
+
+	for(let i = 0;i<word.length;i++){
+		const letter = word[i];
+
+		if(!letterMap.hasOwnProperty(letter)){
+			letterMap[letter] = [];
+		}
+		letterMap[letter].push(i);
+	}
+	return letterMap;
+}
+
+console.log(mapLetters("dodo"));
+console.log(mapLetters("froggy")); 
+console.log(mapLetters("grapes"));
+
